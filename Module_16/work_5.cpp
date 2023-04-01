c79248700c353acca38dfde539e0a2046809f85f
Vladimir Kuligin <vlaku94@yandex.ru>
2023-04-01 18:00:34 +0300

    work 5

--- Module_16/work_5.cpp
+++ Module_16/work_5.cpp
@@ -1,112 +1,137 @@
 /*
-Старая добрая игра на страницах всех школьных тетрадей.
-Поле размером 3 × 3 представлено в виде двумерного массива с типом элементов char.
-Участвуют два игрока, они ходят по очереди и ставят в указанные ими в стандартном вводе координаты свой символ — X или O (буквы латинского алфавита).
-Как только у одного из игроков будет установлено подряд три крестика или три нолика, он побеждает.
-Если свободных клеток не осталось, а трёх совпадающих элементов не найдено, то объявляется ничья.
-Для простоты не будем рассматривать диагональные совпадения — только строго вертикальные и строго горизонтальные.
-
-Изначально всё поле инициализируется символом пробела — ‘ ‘(для обозначения пробела кавычки одинарные и в одну сторону).
-Это можно сделать сразу при объявлении либо с помощью вложенного цикла.
-На каждом ходе при занятой клетке или при неверных координатах этой клетки должно быть выведено сообщение, и игрок должен указать координаты клетки повторно.
-После каждого хода надо выводить в консоль текущее состояние всего игрового поля для наглядности.
+Разработайте упрощённую модель умного дома для дачи. Выполните симуляцию его работы на протяжении двух дней.
+Стартовое время для симуляции умного дома 00:00. Есть несколько датчиков: датчик температуры снаружи, датчик температуры внутри дома.
+В доме расположен электронный умный щиток с автоматами.
  */
 
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
-void ChangePlayer(char& current_player);
-void PrintField(const char field[3][3]);
-char CheckWin(const char field[3][3]);
+enum switches
+{
+    LIGHTS_OUTSIDE = 1,
+    HEATERS = 2,
+    WATER_PIPE_HEATING = 4,
+    CONDITIONER = 8,
+    MOVEMENT = 16,
+    LIGHTS_INSIDE = 32
+};
 
 int main() {
-    char field[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    int current_hour = 0;
+    int temperature_inside = 0;
+    int temperature_outside = 0;
+    std::string movement = "no";
+    std::string light = "no";
+    int light_intensity = 5000;
+    int switches_state = 0;
 
-    int n = 0;
-    char player_win = '-';
-    char current_player = 'X';
+    while(current_hour <= 24){
+        int temp_state = switches_state;
+        std::string buffer;
+        std::cout << "Input info(temp_inside, temp_outside, movement, light: ";
+        std::getline(std::cin, buffer);
 
+        std::stringstream buffer_stream(buffer);
+        buffer_stream >> temperature_inside >> temperature_outside >> movement >> light;
 
-    while(n <= 9 && player_win == '-') {
-        int x = 0;
-        int y = 0;
-        std::cout << "Player " << current_player << " Input coordinates(X and Y): ";
-        std::cin >> x >> y;
-        if (x >= 3 || y >= 3) {
-            std::cout << "Incorrect coordinates. Try again" << std::endl;
-            continue;
-        }
-        if (field[x][y] != ' ') {
-            std::cout << "This cell is occupied. Try again" << std::endl;
-            continue;
-        }
-        field[x][y] = current_player;
-        PrintField(field);
-        player_win = CheckWin(field);
-        ChangePlayer(current_player);
-        ++n;
-    }
-
-    if (player_win == '-') {
-        std::cout << "Nobody" << std::endl;
-    } else if (player_win == 'X') {
-        std::cout << "Winner - " << player_win << std::endl;
-    } else if (player_win == 'O') {
-        std::cout << "Winner - " << player_win << std::endl;
-    }
-
-}
-
-void ChangePlayer(char& current_player) {
-    if (current_player == 'X')  {
-        current_player = 'O';
-    } else {
-        current_player = 'X';
-    }
-}
-
-void PrintField(const char field[3][3]) {
-    for (int row = 0; row < 3; ++row) {
-        for (int column = 0; column < 3; ++column) {
-            std::cout << field[row][column];
-        }
-        std::cout << std::endl;
-    }
-}
-
-char CheckWin(const char field[3][3]) {
-    char win = '-';
-
-    for (int row = 0; row < 3; ++row) {
-        // запоминаем первый символ по горизонтали
-        char horizontal = field[row][0];
-        // запоминаем первый символ по вертикали
-        char vertical = field[0][row];
-        bool horizontal_check = true;
-        bool vertical_check = true;
-
-        for (int column = 0; column < 3; ++column) {
-            if (horizontal != field[row][column] && vertical != field[column][row]) {
-                horizontal_check = false;
-                vertical_check = false;
-                break;
+        if (temperature_outside < 0){
+            if (temp_state != (switches_state |= WATER_PIPE_HEATING)){
+                std::cout << "WATER_PIPE_HEATING - " << "ON" << std::endl;
+                temp_state = switches_state;
             }
-
-            if (horizontal != field[row][column] || horizontal == ' ') horizontal_check = false;
-            if (vertical != field[column][row] || vertical == ' ') vertical_check = false;
-
+        } else if(temperature_outside > 5){
+            if (temp_state != (switches_state &= ~WATER_PIPE_HEATING)){
+                std::cout << "WATER_PIPE_HEATING - " << "OFF" << std::endl;
+                temp_state = switches_state;
+            }
         }
 
-        if (horizontal_check) win = horizontal;
-        if (vertical_check) win = vertical;
+        if (movement == "yes" && (current_hour > 16 || current_hour < 5)){
+            if (temp_state != (switches_state |= LIGHTS_OUTSIDE)){
+                std::cout << "LIGHTS_OUTSIDE - " << "ON" << std::endl;
+                temp_state = switches_state;
+            }
+        } else {
+            if (temp_state != (switches_state &= ~LIGHTS_OUTSIDE)){
+                std::cout << "LIGHTS_OUTSIDE - " << "OFF" << std::endl;
+                temp_state = switches_state;
+            }
+        }
 
-        if(win != '-') break;
+        if (temperature_inside < 22) {
+            if (temp_state != (switches_state |= HEATERS)){
+                std::cout << "HEATERS - " << "ON" << std::endl;
+                temp_state = switches_state;
+            }
+        } else if (temperature_inside >= 25) {
+            if (temp_state != (switches_state &= ~HEATERS)){
+                std::cout << "HEATERS - " << "OFF" << std::endl;
+                temp_state = switches_state;
+            }
+        }
 
+        if  (temperature_inside > 30) {
+            if (temp_state != (switches_state |= CONDITIONER)){
+                std::cout << "CONDITIONER - " << "ON" << std::endl;
+                temp_state = switches_state;
+            }
+        } else if (temperature_inside <= 25) {
+            if (temp_state != (switches_state &= ~CONDITIONER)){
+                std::cout << "CONDITIONER - " << "OFF" << std::endl;
+                temp_state = switches_state;
+            }
+        }
+
+        if (light == "yes") {
+            if (temp_state != (switches_state |= LIGHTS_INSIDE)){
+                std::cout << "LIGHTS_INSIDE - " << "ON" << std::endl;
+                temp_state = switches_state;
+            }
+        } else {
+            if (temp_state != (switches_state &= ~LIGHTS_INSIDE)){
+                std::cout << "LIGHTS_INSIDE - " << "OFF" << std::endl;
+                temp_state = switches_state;
+            }
+        }
+
+        if (movement == "yes") {
+            if (temp_state != (switches_state |= MOVEMENT)){
+                std::cout << "MOVEMENT - " << "YES" << std::endl;
+                temp_state = switches_state;
+            }
+        } else {
+            if (temp_state != (switches_state &= ~MOVEMENT)){
+                std::cout << "MOVEMENT - " << "NO" << std::endl;
+                temp_state = switches_state;
+            }
+        }
+
+        if (light == "yes"){
+            if (current_hour >= 16 && current_hour <= 20) {
+                light_intensity = 5000 - ((current_hour - 16) * ((5000-2700) / (20 - 16)));
+            } else if( current_hour == 23) {
+                light_intensity = 5000;
+            }
+            std::cout << "Color temperature: " << light_intensity << "K" << std::endl;
+        }
+
+        std::cout << current_hour << std::endl;
+
+        ++current_hour;
     }
 
-    return win;
 }
+
+
+
+
+
+
+
+
 
 
 
