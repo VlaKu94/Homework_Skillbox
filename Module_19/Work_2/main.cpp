9b059a767cffe15a6da739dd98acef2a6d8d2476
Vladimir Kuligin <vlaku94@yandex.ru>
2023-04-19 18:41:15 +0300

    work 2

--- Module_19/Work_2/main.cpp
+++ Module_19/Work_2/main.cpp
@@ -5,8 +5,50 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //std::string filePath = "..\\words.txt";
+
+
+    std::ifstream fs;
+    int currentLine = 0;
+    int counterLines = 2;
+    char exit = 'y';
+
+    std::string filePath;
+    std::cout << "Input File Path:";
+    std::cin >> filePath;
+
+
+    fs.open(filePath);
+
+    if (fs.is_open()){
+        while(exit == 'y') {
+            counterLines += currentLine;
+            for (std::string line; std::getline(fs, line);++currentLine) {
+
+                std::cout << line << std::endl;
+                if (currentLine == counterLines) break;
+
+            }
+            if (fs.eof()){
+                std::cout << "File end" << std::endl;
+                break;
+            } else {
+                std::cout << "Continue?(y/n)" << std::endl;
+                std::cin >> exit;
+                currentLine = 0;
+            }
+        }
+
+    } else {
+        std::cout << "Incorrect file path" << std::endl;
+    }
+
+    fs.close();
+
+
 }
